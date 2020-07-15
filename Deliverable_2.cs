10625f2b61740c80da1304f9273e912fe3ac686e
Robbie-kress <68262512+Robbie-kress@users.noreply.github.com>
2020-07-14 22:05:21 -0400

    Add files via upload

---
+++ Deliverable_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Deliverable_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string input;
+            string message = "";
+            int checksum = 0;
+            
+            Console.Write("What is your message? ");
+            input = Console.ReadLine();            
+            input = input.ToUpper();  
+            
+            for (int t = 0; t < input.Length; t++)
+            {
+                checksum += (int)input[t];
+                if (t < input.Length - 1) {                                 
+                    message += ((int)input[t] - 64) + "-";
+                }
+                else {
+                    message += (int)input[t] - 64; }
+            }           
+            Console.WriteLine("Your encoded message is " + message);
+            Console.WriteLine("Message checksum is " + checksum);
+
+        }
+    }
+}
